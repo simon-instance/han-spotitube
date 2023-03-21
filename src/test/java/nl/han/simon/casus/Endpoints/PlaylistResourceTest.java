60b83caa7e517cad83390f65fb634e3f017d6eb8
simon <simonpeters05@gmail.com>
2023-03-21 10:54:20 +0100

    :hammer: Fixing PlaylistService structure

--- src/test/java/nl/han/simon/casus/Endpoints/PlaylistResourceTest.java
+++ src/test/java/nl/han/simon/casus/Endpoints/PlaylistResourceTest.java
@@ -1,15 +1,18 @@
 package nl.han.simon.casus.Endpoints;
 
 import jakarta.ws.rs.core.Response;
-import nl.han.simon.casus.Services.LoginService;
+import nl.han.simon.casus.DAOs.PlaylistDAO;
+import nl.han.simon.casus.DAOs.TrackDAO;
+import nl.han.simon.casus.DTOs.ConvertedPlaylistDTO;
+import nl.han.simon.casus.DTOs.PlaylistsWrapperDTO;
+import nl.han.simon.casus.DTOs.TrackDTO;
 import nl.han.simon.casus.Services.PlaylistService;
 import nl.han.simon.casus.Services.TrackService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import org.mockito.Mockito;
 
 class PlaylistResourceTest {
     private PlaylistResource sut;
@@ -21,12 +24,22 @@ class PlaylistResourceTest {
     public void setup() {
         sut = new PlaylistResource();
 
-        mockedPlaylistService = mock(PlaylistService.class);
-        mockedTrackService= mock(TrackService.class);
+        mockedPlaylistService = new PlaylistService();
+        mockedTrackService = new TrackService();
+
+        var mockedTrackDAO = Mockito.mock(TrackDAO.class);
+
+        var mockedPlaylistDAO = Mockito.mock(PlaylistDAO.class);
+        mockedPlaylistDAO.setTrackDAO(mockedTrackDAO);
+
+        mockedPlaylistService.setPlaylistDAO(mockedPlaylistDAO);
+        mockedPlaylistService.setTrackDAO(mockedTrackDAO);
+        mockedTrackService.setTrackDAO(mockedTrackDAO);
 
         sut.setPlaylistService(mockedPlaylistService);
         sut.setTrackService(mockedTrackService);
     }
+
     @Test
     public void retrievePlaylistsWithTokenString() {
         //arrange
@@ -36,9 +49,20 @@ class PlaylistResourceTest {
         Response res = sut.playlists(tokenString);
 
         //assert
-        assertEquals(200, res.getStatus());
-//        assertEquals(res.getEntity(), mockedPlaylistService.getAllPlaylists());
+        assertEquals(200, res.getEntity());
+    }
 
+    @Test
+    public void retrievePlaylistsWithTokenString_Service() {
+        //arrange
+        String tokenString = "1234-1234-1234";
+        var expected = new PlaylistsWrapperDTO();
+
+        //act
+        Response res = Mockito.when(mockedPlaylistService.getAllPlaylists()).thenReturn(expected);
+
+        //assert
+        assertEquals(200, res.getEntity());
     }
 
     @Test
@@ -64,7 +88,61 @@ class PlaylistResourceTest {
 
         //assert
         assertEquals(200, res.getStatus());
-//        assertEquals(res.getEntity(), mockedTrackService.getTracksByPlaylist(1));
+    }
+
+    @Test
+    public void addTrackToPlaylistWithTokenString() {
+        //arrange
+        String tokenString = "1234-1234-1234";
+
+        var track = new TrackDTO();
+        track.setTitle("testTrack");
+        track.setOfflineAvailable(false);
+        track.setPerformer("performer");
+        track.setPublicationDate("2002-02-02");
+        track.setPlaycount(30);
+        track.setDescription("bami");
+        track.setAlbum("testAlbum");
+
+        //act
+        Response res = sut.addTrackToPlaylist(1, tokenString, track);
+
+        //assert
+        assertEquals(res.getStatus(), 303);
+    }
+
+    @Test
+    public void updatePlaylistNameWithTokenString() {
+        //arrange
+        var tokenString = "1234-1234-1234";
+
+        //act
+        var playlistDTO = new ConvertedPlaylistDTO();
+        playlistDTO.setName("testPlaylist");
+
+//        //assert
+//        assertEquals();
+    }
+
+    @Test
+    public void addTrackToPlaylistWithoutTokenString() {
+        //arrange
+        String tokenString = null;
+
+        var track = new TrackDTO();
+        track.setTitle("testTrack");
+        track.setOfflineAvailable(false);
+        track.setPerformer("performer");
+        track.setPublicationDate("2002-02-02");
+        track.setPlaycount(30);
+        track.setDescription("bami");
+        track.setAlbum("testAlbum");
+
+        //act
+        Response res = sut.addTrackToPlaylist(1, tokenString, track);
+
+        //assert
+        assertEquals(res.getStatus(), 403);
     }
 
     @Test
